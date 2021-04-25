3ac4230201a451ea7f53c034fccaed2fc5553eab
Alessandra Ogawa <alessandrayco@sl.insper.edu.br>
2021-04-24 22:05:17 -0300

    segundo commit de funcoes

---
+++ movs_possiveis.py
@@ -0,0 +1,21 @@
+def extrai_naipe(carta):
+    if len(carta)>2:
+        return carta[2]
+    else:
+        return carta[1]
+
+def extrai_valor(carta):
+    if len(carta)>2:
+        return carta[:2]
+    else:
+        return carta[0]
+
+def lista_movimentos_possiveis(baralho, i):
+    posicoes_validas = [1,3]
+    movimentos_possiveis = []
+    for e in posicoes_validas:
+        if i<e:
+            break
+        if extrai_naipe(baralho[i]) == extrai_naipe(baralho[i-e]) or extrai_valor(baralho[i]) == extrai_valor(baralho[i-e]):
+            movimentos_possiveis.append(e)
+    return movimentos_possiveis
