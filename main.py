21261e48246554b120a17012b902f88c1db35e2d
ArthurCisotto <arthurcisottom@gmail.com>
2021-04-27 17:29:59 -0300

    adaptação da função principal do jogo em relação às novas funções

--- main.py
+++ main.py
@@ -20,31 +20,11 @@ print('Desde que alguma das condições acima seja satisfeita, qualquer carta po
 print('Aperte [Enter] para iniciar o jogo...')
 input()
 
-#Randomiza a ordem do baralho#
-baralho = funcoes.cria_baralho()
-random.shuffle(baralho)
 
-#Estado do baralho
-while funcoes.possui_movimentos_possiveis(baralho):
-    print('O estado atual do baralho é:')
-    for e in range(len(baralho)):
-        print(f'{e+1}. {baralho[e]}')
-    carta_escolhida = int(input(f'Escolha uma carta (digite um número entre 1 e {len(baralho)}):'))
+def main():
+    funcoes.play()
+    while input("Jogar novamente (S/N) ").upper() == "S":
+        funcoes.play()
 
-        #Checar os movimentos possíveis
-    if funcoes.lista_movimentos_possiveis(baralho, carta_escolhida-1) == []:
-        print(f'A carta {baralho[carta_escolhida-1]} não pode ser movida. Por favor, digite um número entre 1 e {len(baralho)}:')
-    if funcoes.lista_movimentos_possiveis(baralho, carta_escolhida-1) == [1, 3]:
-        print(f'Sobre qual carta você quer empilhar o {baralho[carta_escolhida-1]}?')
-        print(f'1. {baralho[carta_escolhida-2]}')
-        print(f'2. {baralho[carta_escolhida-4]}\n')
-        escolha = int(input('Digite o número de sua escolha (1-2):'))
-        if escolha == 1 :
-            funcoes.empilha(baralho, carta_escolhida-1, carta_escolhida-2)
-        if escolha == 2:
-            funcoes.empilha(baralho, carta_escolhida-1, carta_escolhida-4)
-    if funcoes.lista_movimentos_possiveis(baralho, carta_escolhida-1) == [1]:
-        funcoes.empilha(baralho, carta_escolhida-1, carta_escolhida-2)
-    if funcoes.lista_movimentos_possiveis(baralho, carta_escolhida-1) == [3]:
-        funcoes.empilha(baralho, carta_escolhida-1, carta_escolhida-4)
-
+if __name__ == "__main__":
+    main()
