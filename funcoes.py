ac7c901f840d8391594d1587d7d2761abcd85bf0
ArthurCisotto <arthurcisottom@gmail.com>
2021-05-08 17:17:40 -0300

    mensagem de erro para cartas inválidas escolhidas

--- funcoes.py
+++ funcoes.py
@@ -83,6 +83,7 @@ def play():
     random.shuffle(baralho)
 
     while possui_movimentos_possiveis(baralho):
+        empilhou = False
         print('O estado atual do baralho é:')
         for posicao, carta in enumerate(baralho, 1):
             if extrai_naipe(carta) == '♠':
@@ -93,24 +94,35 @@ def play():
                 print(f'{posicao}. {colors.VERDE}{ascii(carta)}{colors.ENDC}')
             if extrai_naipe(carta) == '♣':
                 print(f'{posicao}. {colors.AMARELO}{ascii(carta)}{colors.ENDC}')
-        carta_escolhida = int(input(f'Escolha uma carta (digite um número entre 1 e {len(baralho)}):'))
-
-        #Checar os movimentos possíveis
-        if lista_movimentos_possiveis(baralho, carta_escolhida-1) == []:
-            print(f'A carta {baralho[carta_escolhida-1]} não pode ser movida. Por favor, digite um número entre 1 e {len(baralho)}:')
-        if lista_movimentos_possiveis(baralho, carta_escolhida-1) == [1, 3]:
-            print(f'Sobre qual carta você quer empilhar o {baralho[carta_escolhida-1]}?')
-            print(f'1. {baralho[carta_escolhida-2]}')
-            print(f'2. {baralho[carta_escolhida-4]}\n')
-            escolha = int(input('Digite o número de sua escolha (1-2):'))
-            if escolha == 1 :
-                empilha(baralho, carta_escolhida-1, carta_escolhida-2)
-            if escolha == 2:
-                empilha(baralho, carta_escolhida-1, carta_escolhida-4)
-        if lista_movimentos_possiveis(baralho, carta_escolhida-1) == [1]:
-            empilha(baralho, carta_escolhida-1, carta_escolhida-2)
-        if lista_movimentos_possiveis(baralho, carta_escolhida-1) == [3]:
-            empilha(baralho, carta_escolhida-1, carta_escolhida-4)
+        
+        while empilhou == False:
+            carta_escolhida = int(input(f'Escolha uma carta (digite um número entre 1 e {len(baralho)}):'))
+            if carta_escolhida > 0 and carta_escolhida <= len(baralho):
+                #Checar os movimentos possíveis
+                if lista_movimentos_possiveis(baralho, carta_escolhida-1) == []:
+                    print(f'A carta {baralho[carta_escolhida-1]} não pode ser movida. Por favor, digite um número entre 1 e {len(baralho)}:')
+                if lista_movimentos_possiveis(baralho, carta_escolhida-1) == [1, 3]:
+                    print(f'Sobre qual carta você quer empilhar o {baralho[carta_escolhida-1]}?')
+                    print(f'1. {baralho[carta_escolhida-2]}')
+                    print(f'2. {baralho[carta_escolhida-4]}\n')
+                    escolha = int(input('Digite o número de sua escolha (1-2):'))
+                    if escolha > 0 and escolha < 3:
+                        if escolha == 1 :
+                            empilha(baralho, (carta_escolhida-1), (carta_escolhida-2))
+                            empilhou = True
+                        if escolha == 2:
+                            empilha(baralho, (carta_escolhida-1), (carta_escolhida-4))
+                            empilhou = True
+                    else:
+                        print("Escolha inválida")
+                if lista_movimentos_possiveis(baralho, carta_escolhida-1) == [1]:
+                    empilha(baralho, carta_escolhida-1, carta_escolhida-2)
+                    empilhou = True
+                if lista_movimentos_possiveis(baralho, carta_escolhida-1) == [3]:
+                    empilha(baralho, carta_escolhida-1, carta_escolhida-4)
+                    empilhou = True
+            else:
+                print("Essa carta não é valida")
 
     if len(baralho) == 1:
         print('Parabéns! Você ganhou!')
